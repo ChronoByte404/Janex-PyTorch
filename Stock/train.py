4059de8d6a704e061ac0d684149e0d44cf05aca0
Cipher <cipher@SAN-Kali1.www.github.com//cipher58>
2023-08-06 13:32:28 +0100

    Update training prog

--- Stock/train.py
+++ Stock/train.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 import nltk
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+from Janex import *
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -32,33 +33,13 @@ class NeuralNet(nn.Module):
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
-    """
-    split sentence into array of words/tokens
-    a token can be a word or punctuation character, or number
-    """
-    return nltk.word_tokenize(sentence)
-
+    return IntentMatcher.tokenize(sentence)
 
 def stem(word):
-    """
-    stemming = find the root form of the word
-    examples:
-    words = ["organize", "organizes", "organizing"]
-    words = [stem(w) for w in words]
-    -> ["organ", "organ", "organ"]
-    """
     return stemmer.stem(word.lower())
 
 
 def bag_of_words(tokenized_sentence, words):
-    """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
-    """
     # stem each word
     sentence_words = [stem(word) for word in tokenized_sentence]
     # initialize bag with 0 for each word
